63bd379e7bab81bb254ed24fa02d7d0cc8dcfbfc
Slava <sh-e-95@mail.ru>
2022-06-29 23:27:31 +0500

    add new commands

--- models/command.py
+++ models/command.py
@@ -10,6 +10,10 @@ def get_command(text, shapes):
         for shape in shapes:
             if shape.name in last_word:
                 ShapeSearch(shape)
+        if last_word == 'валюту':
+            MoneySearch()
+        elif last_word == 'слову':
+            OtherSearch(last_word)
         NothingCommand()
     else:
         NothingCommand()
@@ -22,11 +26,10 @@ class Command(ABC):
 
     @property
     def message(self):
-        pass
+        ''
 
 
 # command for search shapes
-
 class ShapeSearch(Command):
     figure = Circle()
 
@@ -34,17 +37,41 @@ class ShapeSearch(Command):
         self.figure = figure
 
     def execute(self):
-        var = ()
+        pass
         # todo search shape
 
     def message(self):
-        f"ждем фотографию с {self.figure.name}ами"
+        f'ждем фотографию с {self.figure.name}ами'
 
 
 # command that does nothing
 class NothingCommand(Command):
     def execute(self):
-        var = ()
+        pass
 
     def message(self):
-        "Моя твоя не понимать"
+        'Моя твоя не понимать'
+
+
+# command for search money
+class MoneySearch(Command):
+    def execute(self):
+        pass
+
+    def message(self):
+        'ждем фотографию с деньгами'
+
+
+# command for search other objects
+class OtherSearch(Command):
+    text = ''
+
+    def __init__(self, text):
+        self.text = text
+
+    def execute(self):
+        pass
+        # todo search shape
+
+    def message(self):
+        f'ждем фотографию с {self.text}'
