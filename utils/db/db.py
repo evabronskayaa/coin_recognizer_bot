46ac27fd40af6d997c85c62816c1d0db842be532
Slava <sh-e-95@mail.ru>
2022-06-30 15:00:48 +0500

    created directory for bd

--- utils/db/db.py
+++ utils/db/db.py
@@ -1,43 +1,4 @@
-from peewee import *
-
-db = SqliteDatabase('')
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-
-class User(BaseModel):
-    name = TextField()
-    user_id = CharField()
-
-
-class Manager(BaseModel):
-    name = TextField()
-    user_id = CharField()
-
-
-class Admin(BaseModel):
-    name = TextField()
-    user_id = CharField()
-
-
-class Image(BaseModel):
-    url = TextField()
-    user_id = CharField()
-    image_type = TextField()
-    # + archive
-
-
-class Favorites(BaseModel):
-    url = TextField()
-    user_id = CharField()
-
-
-class History(BaseModel):
-    url = TextField()
-    user_id = CharField()
+from utils.db_models.db_models import *
 
 
 def init_db():
