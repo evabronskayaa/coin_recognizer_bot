c0aad89882e4e83ea10fa2c3f7fc5c52ea101e93
Slava <sh-e-95@mail.ru>
2022-07-01 15:05:44 +0500

    Corrected db models

--- utils/db_models/db_models.py
+++ utils/db_models/db_models.py
@@ -9,29 +9,29 @@ class BaseModel(Model):
 
 
 class User(BaseModel):
-    name = CharField()
-    id = PrimaryKeyField(unique=True)
-    start_date = DateTimeField()
-    cash_account = IntegerField()
+    name = CharField(null=False)
+    id = IntegerField(unique=True, null=True, help_text="id that use in the Telegram", primary_key=True)
+    start_date = DateTimeField(null=True)
+    cash_account = IntegerField(null=True)
 
 
 class Manager(BaseModel):
-    token = CharField()
-    user_id = ForeignKeyField(User, unique=True)
+    token = CharField(null=True)
+    user_id = ForeignKeyField(User, unique=True, null=True)
 
 
 class Admin(BaseModel):
-    user_id = ForeignKeyField(User, unique=True)
+    user_id = ForeignKeyField(User, unique=True, null=True)
 
 
 class Request(BaseModel):
-    user_id = ForeignKeyField(User, unique=True)
-    date = DateTimeField()
-    message = TextField()
-    data = BigBitField()
-    id = PrimaryKeyField(unique=True)
+    user_id = ForeignKeyField(User, unique=True, null=True)
+    date = DateTimeField(null=True)
+    message = CharField(null=False)
+    data = BigBitField(null=True)
+    id = IntegerField(unique=True, null=True, help_text="id of Request", primary_key=True)
 
 
 class Follow(BaseModel):
-    user_id = ForeignKeyField(User, unique=True)
-    request_id = ForeignKeyField(Request, unique=True)
+    user_id = ForeignKeyField(User, unique=True, null=True)
+    request_id = ForeignKeyField(Request, unique=True, null=True)
